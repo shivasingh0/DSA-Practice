98fdc83d4aacacfa5e8c2f2c917de87f655bb023
shivasingh0 <shiva2003rajawat@gmail.com>
2023-12-12 15:11:56 +0530

    First que. on reverse str

--- reverseString.js
+++ reverseString.js
@@ -1,3 +1,19 @@
-// Hello World
+// Reverse String
 
-let str = 'hello world';
+let str = 'hello world';  // Que. - 1
+
+let reverseStr = str.split(' ')
+// Method -1
+// let secondReverse = reverseStr[0].split('').reverse().join('')
+// let thirdReverse = reverseStr[1].split('').reverse().join('')
+
+// console.log(secondReverse,thirdReverse);
+
+// Method - 2
+let loopReverse = ""
+for (let index of reverseStr ) {
+    let seconedReverse = index.split('').reverse().join('')
+    loopReverse += seconedReverse
+}
+
+console.log(loopReverse);
